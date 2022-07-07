c046ffffe4887237e2ba1c980746eaed0932d786
KamilAugustyn22 <augustynkamil15@gmail.com>
2022-07-07 18:13:39 +0200

    Number of songs on dashboard

--- src/main/java/pl/coderslab/HomeController.java
+++ src/main/java/pl/coderslab/HomeController.java
@@ -1,14 +1,22 @@
 package pl.coderslab;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
+import pl.coderslab.song.SongRepository;
 
 
 @Controller
 public class HomeController {
+    private final SongRepository songRepository;
+
+    public HomeController(SongRepository songRepository) {
+        this.songRepository = songRepository;
+    }
 
     @RequestMapping("/")
-    public String home(){
+    public String home(Model model){
+        model.addAttribute("liczbaPiosenek",songRepository.countAllSongs());
         return "index";
     }
 }
