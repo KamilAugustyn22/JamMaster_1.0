8eee46064a7c3e73ab60bb70ec733f2a36e9676c
KamilAugustyn22 <augustynkamil15@gmail.com>
2022-07-16 13:49:44 +0200

    removed unnecesary import

--- src/main/java/pl/coderslab/HomeController.java
+++ src/main/java/pl/coderslab/HomeController.java
@@ -1,6 +1,5 @@
 package pl.coderslab;
 
-import org.springframework.boot.web.servlet.server.Session;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.*;
