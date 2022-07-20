761c2c0ac6e41f2b10a6760f44927001801e28b0
KamilAugustyn22 <augustynkamil15@gmail.com>
2022-07-20 20:00:55 +0200

    progress with transposition (DOM found chords)

--- src/main/webapp/js/transposition.js
+++ src/main/webapp/js/transposition.js
@@ -1,7 +1,5 @@
 const buttons = document.querySelectorAll(".transpose-btn");
 let introChords = document.querySelector(".intro-chords");
-let introString = introChords.toString();
-let introArray = introString.split();
 let verseChords = document.querySelector(".verse-chords");
 let reffChords = document.querySelector(".reff-chords");
 let bridgeChords = document.querySelector(".bridge-chords");
@@ -10,11 +8,21 @@ for (const button of buttons){
     button.addEventListener("click", function (event){
         let chordsMajor = ["C","C#","D","D#","E","F","F#","G","G#","A","A#","H"];
         let chordsMinor = ["c","c#","d","d#","e","f","f#","g","g#","a","a#","h"];
-        if(button.value=="+"){
-           console.log("+")
+        let intro = introChords.innerText;
+        let verse = verseChords.innerText;
+        let reffrain = reffChords.innerText;
+        let bridge = bridgeChords.innerText;
+        if(button.value == "+"){
+             console.log(intro + "+");
+             console.log(verse + "+");
+             console.log(reffrain + "+");
+             console.log(bridge + "+");
         }
-        if(button.value=="-"){
-            console.log("-");
+        if(button.value == "-"){
+            console.log(intro + "-");
+            console.log(verse + "-");
+            console.log(reffrain + "-");
+            console.log(bridge + "-");
         }
     })
 }
